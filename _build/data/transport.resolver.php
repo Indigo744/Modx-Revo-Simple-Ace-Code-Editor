91e078a797c65dc37fe24c2d80a00f2871014c77
Guillaume <contact-github@gperrin.fr>
2018-05-09 12:07:53 +0200

    Install/Upgrade: force modx cache refresh

--- _build/data/transport.resolver.php
+++ _build/data/transport.resolver.php
@@ -6,19 +6,27 @@
  * @subpackage build
  */
 $success= true;
-if ($pluginid= $object->get('id')) {
+if ($pluginid = $object->get('id')) {
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
+        
+            /* Attempt to set default element editor */
             $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Attempting to set which_element_editor setting to SimpleAceCodeEditor.');
-            // set SimpleAceCodeEditor as default element editor
             $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'which_element_editor'));
             if ($setting) {
                 $setting->set('value','SimpleAceCodeEditor');
                 $setting->save();
             }
             unset($setting);
+            
+            /* Attempt to refresh cache */
+            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Attempting to refresh manager cache.');
+            $object->xpdo->cacheManager->refresh(array(
+                'context_settings' => array('contexts' => array('mgr'))
+            ));
             break;
+            
         case xPDOTransport::ACTION_UNINSTALL:
             $success= true;
             break;
